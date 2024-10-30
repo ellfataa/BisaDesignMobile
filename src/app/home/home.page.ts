1fa2688f4ed4f7eb0b6b88248411e34720718aa5
elfataa <luthfi.emillulfata@mhs.unsoed.ac.id>
2024-10-30 22:21:34 +0700

    first commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { AuthenticationService } from '../services/authentication.service';
+import { Router } from '@angular/router';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +9,31 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  nama = '';
 
-  constructor() {}
+  constructor(
+    private authService: AuthenticationService,
+    private router: Router,
+    private alertController: AlertController
+  ) {
+    this.nama = this.authService.nama;
+  }
 
+  ngOnInit() {}
+
+  async logout() {
+    this.authService.logout();
+    await this.presentLogoutAlert();
+    this.router.navigateByUrl('/login');
+  }
+
+  async presentLogoutAlert() {
+    const alert = await this.alertController.create({
+      header: 'Logout',
+      message: 'Anda berhasil logout.',
+      buttons: ['OK'],
+    });
+
+    await alert.present();
+  }
 }
